e301e470a672587c054552f54038132261357b92
Yudi Yamane <yudiyamane@gmail.com>
2024-11-07 03:28:02 -0300

    remove prints no loop principal

--- app/Main.hs
+++ app/Main.hs
@@ -18,7 +18,7 @@ import Raylib.Types (Rectangle (Rectangle, rectangle'height, rectangle'width), p
 import Raylib.Util (drawing, whileWindowOpen_, withWindow, managed)
 import Raylib.Util.Colors (black, white)
 
-import Game (printGrid, gameInit, gameUpdate, state'grid, state'remainingBombs, state'finished, state'cnt, state'lose)
+import Game (gameInit, gameUpdate, state'grid)
 import Node (Node(..), bomba)
 import Grid (Grid)
 
@@ -90,90 +90,92 @@ main = do
 
         let scale = 1
         let spriteBombSize = 16
+        let gridOffset = 100
         initialState <- (gameInit 10 "easy")
 
         whileWindowOpen_
           (\state -> do
-            -- 3. Renderizar
+            -- 1. Input
             mouseClicked <- isMouseButtonPressed MouseButtonLeft
             (mouseX, mouseY) <- (\(Vector2 x y) -> (floor x, floor y)) <$> getMousePosition
 
-            let colIndex = (mouseX - 100) `div` spriteBombSize
-                rowIndex = (mouseY - 100) `div` spriteBombSize
-                validClick = mouseClicked && rowIndex >= 0 && colIndex >= 0 &&
-                            rowIndex < length (state'grid state) &&
-                            colIndex < length (head (state'grid state))
+            let col = (mouseX - gridOffset) `div` spriteBombSize
+                row = (mouseY - gridOffset) `div` spriteBombSize
+                validClick = mouseClicked && row >= 0 && col >= 0 &&
+                            row < length (state'grid state) &&
+                            col < length (head (state'grid state))
 
-            -- Update
+            -- 2. Atualizar jogo
             newState <- if validClick
-                        then gameUpdate state rowIndex colIndex
+                        then gameUpdate state row col
                         else return state
+
+            -- 3. Renderizar
             drawing
               ( do
                 clearBackground black
-              
-
 
                 -- Campo visível
-                forM_ (zip [0..] (state'grid newState)) $ \(rowIndex, row) -> 
-                    forM_ (zip [0..] row) $ \(colIndex, _) -> 
+                forM_ (zip [0..] (state'grid newState)) $ \(rowIndex, rowList) -> 
+                    forM_ (zip [0..] rowList) $ \(colIndex, _) -> 
                       ( do
-                          let x = 100 + fromIntegral (colIndex * spriteBombSize)
-                              y = 100 + fromIntegral (rowIndex * spriteBombSize)
+                          let x = fromIntegral (gridOffset + (colIndex * spriteBombSize)) :: Float
+                              y = fromIntegral (gridOffset + (rowIndex * spriteBombSize)) :: Float
                               rect = getRectForVisibleCellSprite (state'grid newState) rowIndex colIndex
                           
                           if visited ((state'grid newState) !! rowIndex !! colIndex) then
                             drawTexturePro texture spriteVisited
-                              (Rectangle x y ((rectangle'width (spriteVisited))*(fromIntegral scale)) 
-                              ((rectangle'height (spriteVisited))*(fromIntegral scale))) 
+                              (Rectangle x y ((rectangle'width (spriteVisited))*scale) 
+                              ((rectangle'height (spriteVisited))*scale)) 
                               (Vector2 0 0) 0 white
                           else
                             drawTexturePro texture spriteNotVisited
-                              (Rectangle x y ((rectangle'width (spriteNotVisited))*(fromIntegral scale)) 
-                              ((rectangle'height (spriteNotVisited))*(fromIntegral scale))) 
+                              (Rectangle x y ((rectangle'width (spriteNotVisited))*scale) 
+                              ((rectangle'height (spriteNotVisited))*scale)) 
                               (Vector2 0 0) 0 white
 
                           drawTexturePro texture rect 
-                            (Rectangle x y ((rectangle'width (rect))*(fromIntegral scale)) 
-                            ((rectangle'height (rect))*(fromIntegral scale))) 
+                            (Rectangle x y ((rectangle'width (rect))*scale) 
+                            ((rectangle'height (rect))*scale)) 
                             (Vector2 0 0) 0 white
                       )
 
                 -- Campo invisível, debug apenas
-                forM_ (zip [0..] (state'grid newState)) $ \(rowIndex, row) -> 
-                    forM_ (zip [0..] row) $ \(colIndex, _) -> 
+                forM_ (zip [0..] (state'grid newState)) $ \(rowIndex, rowList) -> 
+                    forM_ (zip [0..] rowList) $ \(colIndex, _) -> 
                       ( do
-                          let x = 500 + fromIntegral (colIndex * spriteBombSize)
-                              y = 100 + fromIntegral (rowIndex * spriteBombSize)
+                          let x = fromIntegral (500 + (colIndex * spriteBombSize)) :: Float
+                              y = fromIntegral (100 + (rowIndex * spriteBombSize)) :: Float
                               rect = getRectForHiddenCellSprite (state'grid newState) rowIndex colIndex
                           
                           -- if not visited
                           drawTexturePro texture spriteVisited 
-                            (Rectangle x y ((rectangle'width (spriteVisited))*(fromIntegral scale)) 
-                            ((rectangle'height (spriteVisited))*(fromIntegral scale))) 
+                            (Rectangle x y ((rectangle'width (spriteVisited))*scale) 
+                            ((rectangle'height (spriteVisited))*scale)) 
                             (Vector2 0 0) 0 white
 
                           drawTexturePro texture rect 
-                            (Rectangle x y ((rectangle'width (rect))*(fromIntegral scale)) 
-                            ((rectangle'height (rect))*(fromIntegral scale))) 
+                            (Rectangle x y ((rectangle'width (rect))*scale) 
+                            ((rectangle'height (rect))*scale)) 
                             (Vector2 0 0) 0 white
                       )
               )
 
             -- 1. Input
-            putStrLn $ "Remaining bombs: " ++ show (state'remainingBombs newState)
-            putStrLn $ "       Finished: " ++ show (state'finished newState)
-            putStrLn $ "            Cnt: " ++ show (state'cnt newState)
-            putStrLn $ "           Lose: " ++ show (state'lose newState)
-            printGrid (state'grid newState)
-            putStrLn "Enter your move \"row col\" (1/n 1/n):"
-            -- FIXME: condição para você ganhou está sempre considerando vitória
-            if (state'finished newState) && (state'lose newState) then
-              putStrLn "               Voce perdeu!!"
-            else if not (state'lose newState) then
-              putStrLn "               Voce ganhou!!"
-            else 
-              return ()
+            -- putStrLn $ "Remaining bombs: " ++ show (state'remainingBombs newState)
+            -- putStrLn $ "       Finished: " ++ show (state'finished newState)
+            -- putStrLn $ "            Cnt: " ++ show (state'cnt newState)
+            -- putStrLn $ "           Lose: " ++ show (state'lose newState)
+            -- printGrid (state'grid newState)
+            -- putStrLn "Enter your move \"row col\" (1/n 1/n):"
+            -- -- FIXME: condição para você ganhou está sempre considerando vitória
+            -- if (state'finished newState) && (state'lose newState) then
+            --   putStrLn "               Voce perdeu!!"
+            -- else if not (state'lose newState) then
+            --   putStrLn "               Voce ganhou!!"
+            -- else 
+            --   return ()
+
             return newState
           )
           initialState
